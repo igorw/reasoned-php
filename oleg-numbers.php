720288466b4d47b6e6914356fbc3d43133ef1947
Igor Wiedler <igor@wiedler.ch>
2014-05-11 22:06:19 +0200

    high-level explanation of oleg numbers

--- oleg-numbers.php
+++ oleg-numbers.php
@@ -4,8 +4,36 @@ namespace igorw\reasoned;
 
 require 'vendor/autoload.php';
 
-// a bit too much
-// these are the oleg numbers from TRS chapter 7
+// a bit too much (the reasoned schemer chapter 7)
+//
+// this is a relational number system (relational meaning based on
+// logical relations).
+//
+// it works by implementing logic gates, as you would find them in
+// an ALU. the logic gates form a half-adder, and two half adders
+// form a full adder, which is capable of adding two bits with
+// carry-in and carry-out.
+//
+// the actual addero composes n full adders (n being the amount of
+// bits required) to do complete binary arithmetic.
+//
+// pluso is addero with carry-in set to 0. it adds two numbers.
+// minuso is pluso with inverted arguments. it implements subtraction
+// in terms of addition, simply by flipping the arguments.
+//
+// the representation of an oleg number is a little-endian list of
+// bits. essentially a reversed list of binary digits.
+//
+// 0 = ()
+// 1 = (1)
+// 2 = (0 1)
+// 3 = (1 1)
+// 4 = (0 0 1)
+// 5 = (1 0 1)
+// 6 = (0 1 1)
+// 7 = (1 1 1)
+// ...
+//
 // note: running this file requires HHVM
 
 function bit_xoro($x, $y, $r) {
